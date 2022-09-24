945ff40960cc21b264f00f3f35f16ea3a4b427a3
Frederick Mejia <fjdevel@gmail.com>
2022-09-24 04:31:37 -0600

    Agregando modelo de vehiculos y flota

--- Backend/logisticIngenio/src/main/java/com/ingenio/logisticIngenio/models/Flota.java
+++ Backend/logisticIngenio/src/main/java/com/ingenio/logisticIngenio/models/Flota.java
@@ -1,11 +1,22 @@
 package com.ingenio.logisticIngenio.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
+@Data // Create getters and setters
+@NoArgsConstructor
 @Table(name = "flota")
 public class Flota {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Size(min = 8,max = 8)
+    @Column(unique = true, nullable = false)
+    private String numero;
+
 }
--- Backend/logisticIngenio/src/main/java/com/ingenio/logisticIngenio/models/Vehiculo.java
+++ Backend/logisticIngenio/src/main/java/com/ingenio/logisticIngenio/models/Vehiculo.java
@@ -1,11 +1,21 @@
 package com.ingenio.logisticIngenio.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
+@Data // Create getters and setters
+@NoArgsConstructor
 @Table(name = "vehiculo")
 public class Vehiculo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Size(min = 6, max = 6)
+    @Column(unique = true, nullable = false)
+    private String placa;
 }
